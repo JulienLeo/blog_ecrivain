b9929d71fb07d66ae09a53df740ba7caaf32a38c
JuLeo <julien.t.leopold@gmail.com>
2019-04-19 18:25:05 +0200

    Ajout d'un fichier css, d'un header, d'un menu et d'un footer

--- view/frontend/listChaptersView.php
+++ view/frontend/listChaptersView.php
@@ -1,14 +1,13 @@
 <?php ob_start(); ?>
 
-<h1>Billet Simple Pour l'Alaska</h1>
-<h2>par Jean Forteroche</h2>
+
 
 <?php
     while ($data = $chapters->fetch()) {
 ?>
         <div class="listChapters">
-            <p><h3><u><?= romanNumbered($data['id']); ?></u></h3></p>
-            <p><h3><u><?= htmlspecialchars($data['title']); ?></u></h3></p>
+            <h3><u><?= romanNumbered($data['id']); ?></u></h3>
+            <h3><u><?= htmlspecialchars($data['title']); ?></u></h3>
             <p><?= nl2br(htmlspecialchars(substrWords($data['content'], 500))); ?></p>
             <p><em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire la suite... </a></em></p>
             <p><em>Ajouté le <?= $data['addition_date_fr']; ?></em></p>
